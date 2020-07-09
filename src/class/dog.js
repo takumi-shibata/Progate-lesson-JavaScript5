debf23712f5fe14c773878d99716589ed9388dd3
takumi-shibata <241448ta@gmail.com>
2020-07-09 17:25:53 +0900

    2周目学習終了

--- src/class/dog.js
+++ src/class/dog.js
@@ -1,11 +1,11 @@
 // Animalクラスをインポートしてください
 // 他のファイルで定義されているクラスを使用するにはインポートする必要がある。
-// 使用するファイルの先頭で「import クラス名 from "./ファイル名";」と記述する。
+// 使用するファイルの先頭で「import クラス名 from "./ファイル名";」と記述する。 ※ファイル名の拡張子の「.js」は省略することができる
 import Animal from "./animal.js";
 
 // chalkパッケージをインポートしてください
-// パッケージ（作成された便利なプログラム(今回はchalk))の導入にもimportを使う
-// 「import 定数名 from "プログラム名"；」
+// パッケージ（作成された便利なプログラム(今回はchalk：文字色を変える))の導入にもimportを使う
+// 「import 定数名 from "パッケージ名"；」
 import chalk from "chalk";
 
 class Dog extends Animal {
@@ -18,6 +18,7 @@ class Dog extends Animal {
     this.greet();
     
     // console.logの中身をchalkを使って書き換えてください
+    // 文字列を「()」で囲む
     console.log(chalk.yellow(`名前は${this.name}です`));
     
     // console.logの中身をchalkを使って書き換えてください
--- src/data/dogData.js
+++ src/data/dogData.js
@@ -1,18 +1,20 @@
-// 相対パス(../ひとつ上の階層に戻る)
+// 相対パス(../ひとつ上の階層に戻る(今回の場合dataフォルダーの階層に戻り、classフォルダーのdogファイルに移動する))
 import Dog from "../class/dog";
 
 // パッケージreadline-syncをインポートしてください
 // パッケージ（作成された便利なプログラム(今回はreadlineSync))の導入にもimportを使う
-// 「import 定数名 from "プログラム名"；」
+// 「import 定数名 from "パッケージ名"；」
 import readlineSync from "readline-sync";
 
 const dog1 = new Dog("レオ", 4, "チワワ");
 const dog2 = new Dog(name, age, breed);
 
 // readlineSync.question(文字の場合)を使って書き換えてください
+// 文字の場合は「question」　　　　　　 ↓文字列を「()」で囲む↓
 const name = readlineSync.question("名前を入力してください: ");
 
 // readlineSync.questionInt(数字の場合)を使って書き換えてください
+// 整数の場合は「questionInt」          ↓文字列を「()」で囲む↓
 const age = readlineSync.questionInt("年齢を入力してください: ");
 
 // readlineSync.questionを使って書き換えてください
